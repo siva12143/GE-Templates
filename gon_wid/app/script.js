96dcf5bdf89faeb37a4d93ef863e63a70eed8af3
siva12143 <siva121435@gmail.com>
2025-02-20 16:50:16 +0530

    update

--- gon_wid/app/script.js
+++ gon_wid/app/script.js
@@ -7,7 +7,7 @@ ZOHO.CREATOR.init()
         let getAllAcceseries = [];
         let getAllFabric = [];
 
-
+        let gonItem = [];
         var queryParams = await ZOHO.CREATOR.UTIL.getQueryParams();
         console.log(queryParams);
 
@@ -174,28 +174,28 @@ ZOHO.CREATOR.init()
             let GONID = "";
             let getCount = 0;
             console.log(setCreiteria);
-            config = {
+            var config2 = {
                 appName: "girish-exports",
-                    reportName: "All_Goods_Outwards_Notes",
-                    criteria: setCreiteria,
+                reportName: "All_Goods_Outwards_Notes",
+                criteria: setCreiteria,
             }
-            
-            ZOHO.CREATOR.API.getRecordCount(config).then(function (response) {
+
+            await ZOHO.CREATOR.API.getRecordCount(config2).then(function (response) {
                 //your code goes here.
                 console.log(response.result.records_count);
                 getCount = response.result.records_count;
-                
-            });
-            let totalIssued = 0;
 
-            if(setCreiteria.length > 0 && getCount > 0){
+            });
+            
+            console.log(setCreiteria, getCount);
+            if (setCreiteria.length > 0 && getCount > 0) {
                 var config = {
                     appName: "girish-exports",
                     reportName: "All_Goods_Outwards_Notes",
                     criteria: setCreiteria,
                     page: 1,
                     pageSize: 200
-                }    
+                }
                 await ZOHO.CREATOR.API.getAllRecords(config).then(async function (response) {
                     console.log(response.data);
                     for (let i = 0; i < response.data.length; i++) {
@@ -206,9 +206,9 @@ ZOHO.CREATOR.init()
                     }
                 })
 
-                let gonItem = [];
-                console.log(GONID);
                 
+                console.log(GONID);
+
                 config = {
                     appName: "girish-exports",
                     reportName: "GON_Items",
@@ -219,19 +219,14 @@ ZOHO.CREATOR.init()
                 await ZOHO.CREATOR.API.getAllRecords(config).then(async function (response) {
                     gonItem = response.data;
                 })
-                
-                getfilterGON = gonItem.filter(obj => obj.Items.ID == e.Item_ID && obj.Size.ID == e.Size_ID);
-                getfilterGON.forEach(e => {
-                    totalIssued += parseFloat(e.GON_Qty || 0);
-                })
+                console.log(gonItem);
             }
-            
-            
+
+
 
 
 
             const mergeArray = [...getAllAcceseries, ...getAllFabric];
-            // console.log(mergeArray);
             const onlyForCategory = [];
             const filterByItemsAndSize = (data) => {
                 return Object.values(
@@ -253,32 +248,36 @@ ZOHO.CREATOR.init()
 
             const result = filterByItemsAndSize(mergeArray);
             let productData = {};
+            console.log(mergeArray);
+
             let totalCount = 0, copy = 0;
             mergeArray.forEach(e => {
                 
+                
+                let requiredQty = parseInt(e.Total_Required_Qty) || 0;
                 let key = `${e.Items.ID}_${e.Size.ID}_${e.Consumption_Unit.ID}`;
                 if (!productData[key]) {
+
                     productData[key] = {
                         Consumption_Unit: e.Consumption_Unit.display_value,
                         Consumption_ID: e.Consumption_Unit.ID,
                         Size: e.Size.display_value,
+                        Unit: e.Consumption_Unit.ID,
                         Size_ID: e.Size.ID,
                         Item: e.Items.display_value,
                         Item_ID: e.Items.ID,
                         Category: e.Item_Category.display_value,
                         Category_ID: e.Item_Category.ID,
-                        Qty: parseInt(e.Total_Required_Qty) || 0,
+                        Qty: requiredQty,
                     }
                 } else {
-                    productData[key].Qty += e.Consumption
+                    productData[key].Qty += requiredQty
                     copy += 1;
                 }
                 totalCount += 1;
             });
             const getAllVal = Object.values(productData);
-            // console.log(getAllVal[0]);
-            // console.log(totalCount, copy);
-            console.log(mergeArray);
+            console.log(getAllVal);
             iterateTable(getAllVal);
 
             const getCategory = document.getElementById("itemCategory");
@@ -290,20 +289,24 @@ ZOHO.CREATOR.init()
                 if (getFilterVal.length == 0) {
                     getFilterVal = getAllVal;
                 }
+                console.log(getFilterVal);
 
                 iterateTable(getFilterVal);
             })
 
             function iterateTable(val) {
+                let totalIssued = 0;
                 const cateList = [];
                 const getCategory = document.getElementById("itemCategory");
 
                 const subform = document.getElementById("tablebody");
                 subform.innerHTML = "";
                 val.forEach(async (e, i) => {
-                    console.log(e);
-
-
+                getfilterGON = gonItem.filter(obj => obj.Items.ID == e.Item_ID && obj.Size.ID == e.Size_ID);
+                console.log(getfilterGON[0]);
+                const filterVal = getfilterGON[0]?.GON_Qty;
+                totalIssued = filterVal
+                console.log(filterVal);
                     // for item category
                     if (!cateList.includes(e.Category_ID) || cateList.length == 0) {
                         cateList.push(e.Category_ID);
@@ -312,9 +315,9 @@ ZOHO.CREATOR.init()
                         option.textContent = e.Category;
                         getCategory.appendChild(option);
                     }
-                
+
                     //    totalIssued = totalIssued.toFixed(4);
-                      console.log(ourRef.length);
+                    console.log(ourRef.length);
                     console.log(ourRef);
                     var config2 = {
                         appName: "girish-exports",
@@ -332,43 +335,49 @@ ZOHO.CREATOR.init()
 
 
                     // for item category
+                    if (e.Qty > totalIssued) {
 
 
-                    const row = document.createElement("tr");
-                    row.id = "row" + i;
-                    row.innerHTML = `
-                    <td  class="border-t-2 border-gray-200 p-2" >
-                      <span id="${i}" class="cursor-pointer deleteRow">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" style="color:red" class=" size-6">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                        </svg>
-                      </span>
-                  </td>
-                  <td class="border-t-2 border-gray-200 p-2">
-                      <select id="item${i}"class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                          <option selected value="${e.Item_ID || ""} ">${e.Item || "Select"}</option>
-                      </select>
-                  </td>
-                  <td  class="w-[250px] border-t-2 border-gray-200 p-2"><iframe id="image${i}" src="" scrolling="no" frameborder="0" allowfullscreen=true width="100%"  title="Embed code" ></iframe></td>
-                  <td  class="border-t-2 border-gray-200 p-2">
-                      <div class="form-group w-full p-4">
-                          <select id="size${i}"class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" disabled>
-                              <option selected value="${e.Size_ID || ""}">${e.Size || ""}</option>
-                          </select>
-                      </div>
-                  </td>
-                  <td  class="border-t-2 border-gray-200 p-2">
-                      <input type="text" id="orderQty${i}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="${e.Qty}" disabled />
-                  </td>
-                  <td  class="border-t-2 border-gray-200 p-2">
-                      <input type="text" id="gonQty${i}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="${totalIssued}" />
-                  </td>
-                  <td  class="border-t-2 border-gray-200 p-2">
-                      <input type="text" id="issueQty${i}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="" />
-                  </td>
-                  
-                  `;
-                    subform.appendChild(row);
+                        const row = document.createElement("tr");
+                        row.id = "row" + i;
+                        row.innerHTML = `
+                            <td  class="border-t-2 border-gray-200 p-2" >
+                            <span id="${i}" class="cursor-pointer deleteRow">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" style="color:red" class=" size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                </svg>
+                            </span>
+                        </td>
+                        <td class="border-t-2 border-gray-200 p-2">
+                            <select id="item${i}"class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected value="${e.Item_ID || ""} ">${e.Item || "Select"}</option>
+                            </select>
+                        </td>
+                        <td  class="w-[250px] border-t-2 border-gray-200 p-2"><iframe id="image${i}" src="" scrolling="no" frameborder="0" allowfullscreen=true width="100%"  title="Embed code" ></iframe></td>
+                        <td  class="border-t-2 border-gray-200 p-2">
+                            <div class="form-group w-full p-4">
+                                <select id="size${i}"class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" disabled>
+                                    <option selected value="${e.Size_ID || ""}">${e.Size || ""}</option>
+                                </select>
+                            </div>
+                        </td>
+                        <td  class="hidden">
+                            <input type="text" id="unit${i}" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="${e.Unit}" disabled />
+                        </td>
+                        <td  class="border-t-2 border-gray-200 p-2">
+                            <input type="text" disabled id="orderQty${i}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="${e.Qty}"  />
+                        </td>
+                        <td  class="border-t-2 border-gray-200 p-2">
+                            <input type="text" id="gonQty${i}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="${totalIssued}" />
+                        </td>
+                        <td  class="border-t-2 border-gray-200 p-2">
+                            <input type="text" id="issueQty${i}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                           value="" />
+                        </td>
+                        
+                        `;
+                        subform.appendChild(row);
+                    }
 
                 })
             }
@@ -397,13 +406,22 @@ ZOHO.CREATOR.init()
 
                 const tablebody = document.getElementById("tablebody");
                 const tablelength = tablebody.getElementsByTagName("tr").length;
-                console.log("Plan Sheet", planSheet, "fromWarehouse", fromWarehouse, "toWarehouse", toWarehouse, "Order", Order.children[0], "item Category", itemCategory);
-                console.log(subform);
+                // console.log("Plan Sheet", planSheet, "fromWarehouse", fromWarehouse, "toWarehouse", toWarehouse, "Order", Order.children[0], "item Category", itemCategory);
+                // console.log(subform);
                 const allEmpty = [];
                 // check null or empty values
 
                 for (let i = 0; i < tablelength; i++) {
-                    const getVal = document.getElementById("issueQty" + i).value || null;
+                    console.log("issueQty" + i);
+                    let getRowID = tableBody.rows[i].id;
+                    getRowID = getRowID.slice(3);
+                    console.log(getRowID);
+
+
+
+                    console.log(document.getElementById("issueQty" + getRowID));
+                    const getVal = document.getElementById("issueQty" + getRowID).value || null;
+
                     if (getVal == null) {
                         allEmpty.push(false);
                     } else {
@@ -442,13 +460,16 @@ ZOHO.CREATOR.init()
                     ZOHO.CREATOR.API.addRecord(config).then(async function (response) {
                         if (response.data.ID) {
                             for (let i = 0; i < tablelength; i++) {
+                                let getRowID = tableBody.rows[i].id;
+                                getRowID = getRowID.slice(3);
                                 const newObj = {
                                     data: {
-                                        Items: document.getElementById("item" + i).value,
-                                        Size: document.getElementById("size" + i).value,
-                                        Order_Qty: document.getElementById("orderQty" + i).value,
-                                        Issued_Qty: document.getElementById("issueQty" + i).value,
-                                        GON_Qty: document.getElementById("gonQty" + i).value,
+                                        Items: document.getElementById("item" + getRowID).value,
+                                        Size: document.getElementById("size" + getRowID).value,
+                                        Order_Qty: document.getElementById("orderQty" + getRowID).value,
+                                        Issued_Qty: document.getElementById("issueQty" + getRowID).value,
+                                        GON_Qty: document.getElementById("gonQty" + getRowID).value,
+                                        Units: document.getElementById("unit" + i).value,
                                         Goods_Outwards_Notes: response.data.ID,
                                     }
                                 }
@@ -464,7 +485,7 @@ ZOHO.CREATOR.init()
                                 })
 
                             }
-                            location.reload();
+                            // location.reload();
                         }
                     });
                 }
